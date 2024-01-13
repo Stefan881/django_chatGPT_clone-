974ec551ef5cfa7bd51dab6c5d929eddfe25ab5f
Ondeyo Stephen <ondeyostephen0@gmail.com>
2024-01-13 16:41:38 +0300

    configured the openai

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .import views
-
+from . import views
 
 urlpatterns = [
-    path('',views.chatbot,name='chatbot'),
+    path('', views.chatbot, name='chatbot'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
+    path('logout', views.logout, name='logout'),
 ]
+
+
--- chatbot/views.py
+++ chatbot/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import openai
+from django.contrib import auth
 
 # Create your views here.
+
+openai_api_key = "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"
+openai.api_key = openai_api_key
+
+
+def ask_openai(message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+
+    answer = response.choice[0].text.strip()
+    return answer
+
+
 def chatbot(request):
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = "Hi this is my response"
+        response = ask_openai(message)
         return JsonResponse({"message": message, "response": response})
     return render(request, 'chatbot.html', {})
+
+
+def login(request):
+    return render(request, 'login.html', {})
+
+
+def register(request):
+    return render(request, 'register.html', {})
+
+
+def logout(request):
+    auth.logout(request)
+
